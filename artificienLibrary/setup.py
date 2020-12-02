e9891946e2499b7f4e5742b07461b208cc4b7e4b
epsteinj <epsteinj.us@gmail.com>
2020-12-02 18:29:25 -0500

    adding dependencies to the library so that pip install handles all required dependences, adding fixes and some fn documentation

--- artificienLibrary/setup.py
+++ artificienLibrary/setup.py
@@ -7,7 +7,13 @@ setup(
         description='Library for artificien, abstracts federated learning processes',
         author='Jake Epstein',
         license='MIT',
-        install_requires=[],
+        install_requires=[
+            'syft==0.2.9',
+            'websocket-client==0.57.0',
+            'torch',
+            'requests',
+            'boto3'
+        ],
         setup_requires=['pytest-runner'],
         tests_require=['pytest==4.4.1'],
         test_suite='tests',

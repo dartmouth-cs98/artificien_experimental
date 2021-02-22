9db406baa1a7812181cb5be30aa82b2b26983f1b
epsteinj.us@gmail.com <jakeepstein@Jakes-MacBook-Pro-272.local>
2021-02-22 18:36:45 -0500

    updated with auth/master working

--- artificienLibrary/artificienlib/constants.py
+++ artificienLibrary/artificienlib/constants.py
@@ -1,4 +1,4 @@
-#gridAddress = "perce-pygri-1wlav2s6qfqom-238e5e536ca4e235.elb.us-east-1.amazonaws.com:5000"
-gridAddress = "pygri-pygri-17y41i49ftf9q-3a24143e22adbdc3.elb.us-east-1.amazonaws.com:5000"
 region_name = "us-east-1"
-masterNode = "127.0.0.1:5000"
+masterNode = "0.0.0.0:5001"
+userPoolId = "us-east-1_gxjsNZ82v"
+clientId = "5nrhkbejfsq8mgi5jc08586maa"
